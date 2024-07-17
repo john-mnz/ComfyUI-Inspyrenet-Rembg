203979ae8cc5281f245f883aaab5fd1343c6e667
unknown <john.mnz.john@gmail.com>
2024-07-17 15:02:02 -0700

    node mapping update

--- __init__.py
+++ __init__.py
@@ -7,4 +7,4 @@ NODE_CLASS_MAPPINGS = {
 NODE_DISPLAY_NAME_MAPPINGS = {
     "InspyrenetRembg": "Inspyrenet Rembg"
 }
-__all__ = ['NODE_CLASS_MAPPINGS']
+__all__ = ['NODE_CLASS_MAPPINGS', "NODE_DISPLAY_NAME_MAPPINGS"]
