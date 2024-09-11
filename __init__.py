52b216ee021a4f8203d78c46feacf9efada70089
807502278 <807502278@qq.com>
2024-09-12 01:09:28 +0800

    Update __init__.py
    Update nodes

--- __init__.py
+++ __init__.py
@@ -1,12 +1,14 @@
-from .Inspyrenet_Rembg import InspyrenetRembg, InspyrenetRembgAdvanced
+from .Inspyrenet_Rembg import InspyrenetRembg, InspyrenetRembgAdvanced, InspyrenetRembg_selectmodel
 
 NODE_CLASS_MAPPINGS = {
     "InspyrenetRembg" : InspyrenetRembg,
     "InspyrenetRembgAdvanced" : InspyrenetRembgAdvanced,
+    "InspyrenetRembg_selectmodel" : InspyrenetRembg_selectmodel
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "InspyrenetRembg": "Inspyrenet Rembg",
-    "InspyrenetRembgAdvanced": "Inspyrenet Rembg Advanced"
+    "InspyrenetRembgAdvanced": "Inspyrenet Rembg Advanced",
+    "InspyrenetRembg_selectmodel": "InspyrenetRembg_selectmodel"
 }
 __all__ = ['NODE_CLASS_MAPPINGS', "NODE_DISPLAY_NAME_MAPPINGS"]
